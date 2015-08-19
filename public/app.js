573f13211ec857419e8ac0743352c4b3e0d6b4b3
Orion Henry <orion.henry@gmail.com>
2015-08-19 07:59:42 -0500

    baby steps

--- public/app.js
+++ public/app.js
@@ -35,3 +35,93 @@ function tabulate(data, columns) {
 
     return table;
 }
+
+function plot(data, input, output) {
+  var margin = {top: 20, right: 20, bottom: 30, left: 40},
+      width = 920 - margin.left - margin.right,
+      height = 470 - margin.top - margin.bottom;
+
+  var x = d3.scale.linear().range([0, width]);
+
+  var y = d3.scale.linear().range([height, 0]);
+
+  var color = d3.scale.category10();
+
+  var xAxis = d3.svg.axis()
+      .scale(x)
+      .orient("bottom");
+
+  var yAxis = d3.svg.axis()
+      .scale(y)
+      .orient("left");
+
+  var svg = d3.select("svg").remove()
+  var svg = d3.select("div#plot").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    x.domain(d3.extent(data, function(d) { return d[output]; })).nice();
+    y.domain(d3.extent(data, function(d) { return d[input]; })).nice();
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
+      .append("text")
+        .attr("class", "label")
+        .attr("x", width)
+        .attr("y", -6)
+        .style("text-anchor", "end")
+        .text(output);
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+      .append("text")
+        .attr("class", "label")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text(input)
+
+    svg.selectAll(".dot")
+        .data(data)
+      .enter().append("circle")
+        .attr("class", "dot")
+        .attr("r", 3.5)
+        .attr("cx", function(d) { return x(d[output]); })
+        .attr("cy", function(d) { return y(d[input]); })
+//        .style("fill", function(d) { return color(d.species); });
+        .style("fill", function(d) { return color("derka"); })
+        .on("click", function(d) {
+          console.log(d)
+          alert(d.Name)
+        })
+
+    var legend = svg.selectAll(".legend")
+        .data(color.domain())
+      .enter().append("g")
+        .attr("class", "legend")
+        .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+    legend.append("rect")
+        .attr("x", width - 18)
+        .attr("width", 18)
+        .attr("height", 18)
+        .style("fill", color);
+
+    legend.append("text")
+        .attr("x", width - 24)
+        .attr("y", 9)
+        .attr("dy", ".35em")
+        .style("text-anchor", "end")
+        .text(function(d) { return d; });
+}
+
+function round2(x) {
+  return Math.round((+x)*100)/100
+}
+
