4d66d7e97fce4729b18929d571756bc3cd7eb1d2
Orion Henry <orion.henry@gmail.com>
2015-10-04 21:42:00 -0500

    include ace editor for markdown

--- src/notebook.jsx
+++ src/notebook.jsx
@@ -1,7 +1,7 @@
-var $ = require("jquery")
-var jQuery = require("jquery")
-var React = require("react")
-var marked = require("marked")
+var $         = require("jquery")
+var React     = require("react")
+var marked    = require("marked")
+var AceEditor = require('react-ace');
 
 var cursorCell = 0;
 
@@ -29,84 +29,94 @@ function moveCursor(delta) {
   }
 }
 
-$(document).ready(function() {
-  $('body').keypress(function(e) {
-    switch (e.which) {
-      case 113:
-        moveCursor(-1);
-        break;
-      case 97:
-        moveCursor(1);
-        break;
-    }
-    e.preventDefault();
-  });
-
-  var mountNode = document.getElementById('mount')
-
-  var MarkdownCell = React.createClass({
-    rawMarkup: function() {
-      var raw = marked(this.props.data.source.join(""), {sanitize: true})
-      return { __html: raw }
-    },
-    render: function() {
-      return <div className="cell" dangerouslySetInnerHTML={this.rawMarkup()} />
-    }
-  });
-
-  var CodeCell = React.createClass({
-    render: function() {
-      var outputs = this.props.data.outputs.map(function(output) {
-        var html = output.data["text/html"]
-        var text = output.data["text/plain"]
-        var png  = output.data["image/png"]
-        if (html) {
-          var code = {__html: html.join("") }
-          return <div dangerouslySetInnerHTML={code} />
-        } else if (png) {
-          var inline = "data:image/png;base64," + png 
-          return <img src={inline} />
-        } else if (text) {
-          var output = text.join("\n")
-          if (output == "''") { // strange :-(
-            return ""
-          } else { 
-            return output
-          }
-        } else {
-          return "UNKNOWN"
-        }
-      }) 
-      return (<div className="cell">
-        <div className="cell-label">In [{this.props.index}]:</div>
-        <div className="codewrap">
-          <div className="code">{this.props.data.source.join("")}</div>
-        </div>
-        <div className="yields"><img src="yield-arrow.png" alt="yields" /></div>
-        <div className="cell-label">Out[{this.props.index}]:</div>
-          {outputs}
-        </div>)
-    }
-  });
-
-  var Notebook = React.createClass({
-    render: function() {
-      console.log(this.props.data)
-      var index = -1;
-      var cells = this.props.data.cells.map(function(cell) {
-        index += 1
-        if (cell.cell_type == "markdown") {
-          return <MarkdownCell data={cell} />
-        } else { 
-          return <CodeCell data={cell} index={index}/>
-        }
-      })
-      return <div className="notebook">{cells}</div>
-    },
-  })
-
-  jQuery.get("waldo.ipynb",function(data) {
-    React.render(<Notebook data={data} />, mountNode);
-    moveCursor(0);
-  }, "json")
+/*
+$('body').keypress(function(e) {
+  switch (e.which) {
+    case 113:
+      moveCursor(-1);
+      break;
+    case 97:
+      moveCursor(1);
+      break;
+  }
+  e.preventDefault();
 });
+*/
+
+var mountNode = document.getElementById('mount')
+
+var onChange = function(x) {
+  console.log("change")
+}
+
+var iii = 0
+var MarkdownCell = React.createClass({
+  rawMarkup: function() {
+    var raw = marked(this.props.data.source.join(""), {sanitize: true})
+    return { __html: raw }
+  },
+  render: function() {
+    iii = iii + 1
+    return (
+      <div>
+        <div className="cell" dangerouslySetInnerHTML={this.rawMarkup()} />
+        <AceEditor mode="markdown" width="100%" value={this.props.data.source.join("\n")} theme="github" onChange={onChange} name={"edit" + iii} editorProps={{$blockScrolling: true}} />
+      </div>)
+  }
+});
+
+var CodeCell = React.createClass({
+  render: function() {
+    var outputs = this.props.data.outputs.map(function(output) {
+      var html = output.data["text/html"]
+      var text = output.data["text/plain"]
+      var png  = output.data["image/png"]
+      if (html) {
+        var code = {__html: html.join("") }
+        return <div dangerouslySetInnerHTML={code} />
+      } else if (png) {
+        var inline = "data:image/png;base64," + png
+        return <img src={inline} />
+      } else if (text) {
+        var output = text.join("\n")
+        if (output == "''") { // strange :-(
+          return ""
+        } else {
+          return output
+        }
+      } else {
+        return "UNKNOWN"
+      }
+    })
+    return (<div className="cell">
+      <div className="cell-label">In [{this.props.index}]:</div>
+      <div className="codewrap">
+        <div className="code">{this.props.data.source.join("")}</div>
+      </div>
+      <div className="yields"><img src="yield-arrow.png" alt="yields" /></div>
+      <div className="cell-label">Out[{this.props.index}]:</div>
+        {outputs}
+      </div>)
+  }
+});
+
+var Notebook = React.createClass({
+  render: function() {
+    console.log(this.props.data)
+    var index = -1;
+    var cells = this.props.data.cells.map(function(cell) {
+      index += 1
+      if (cell.cell_type == "markdown") {
+        return <MarkdownCell data={cell} />
+      } else {
+        return <CodeCell data={cell} index={index}/>
+      }
+    })
+    return <div className="notebook">{cells}</div>
+  },
+})
+
+$.get("waldo.ipynb",function(data) {
+  React.render(<Notebook data={data} />, mountNode);
+  moveCursor(0);
+}, "json")
