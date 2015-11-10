221eacd501f02633463efd42e5522d5b2968208c
Orion Henry <orion.henry@gmail.com>
2015-11-10 15:09:05 -0600

    more tests

--- skulpt/src/builtin/test.py
+++ skulpt/src/builtin/test.py
@@ -2,12 +2,10 @@
 import pandas as pd
 
 
-def make_df():
-    return pd.DataFrame({"head":["h1","h2"],"body":{"h1":[1,2,None,None],"h2":[10,None,30,40]},"length":4})
 
 def test_dropna():
-    print "testing dropna...";
-    df = make_df()
+    print "testing dropna..."
+    df = pd.DataFrame({"head":["h1","h2"],"body":{"h1":[1,2,None,None],"h2":[10,None,30,40]},"length":4})
     df2 = df.dropna(subset=["h1"])
     df3 = df.dropna(subset=["h2"])
     df4 = df.dropna(subset=["h2","h1"])
@@ -16,12 +14,17 @@ def test_dropna():
     assert df4.length == 1
 
 def test_dataframe():
-    print "testing dataframe...";
-    df = make_df()
+    print "testing dataframe...!"
+    df = pd.DataFrame({"head":["h1","h2"],"body":{"h1":[1,2,3,4],"h2":[10,20,30,40]},"length":4})
     assert df.length == 4
+    assert df[0] == (1,10)
+    assert df.h1[0] == 1
+    assert df.h1[1] == 2
+    assert df["h1"][0] == 1
+    assert df["h2"][1] == 20
 
 def run():
-    print "begin testing";
+    print "begin testing"
     test_dataframe()
     test_dropna()
     print "done"
