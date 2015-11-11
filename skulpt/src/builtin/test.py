29cd8218207024af888eec2ab5539d49d6c2d07d
Orion Henry <orion.henry@gmail.com>
2015-11-11 12:34:34 -0600

    better testing output when tests fail

--- skulpt/src/builtin/test.py
+++ skulpt/src/builtin/test.py
@@ -2,30 +2,45 @@
 import pandas as pd
 
 
+class Test:
+    def test_index(self):
+        print "testing index..."
+        df = pd.DataFrame({"head":["h1","h2"],"body":{"h1":[1,2,3,4],"h2":[10,20,30,40]},"length":4})
+#        df2 = df[df["h1"] == "2"]
+#        len(df2)
 
-def test_dropna():
-    print "testing dropna..."
-    df = pd.DataFrame({"head":["h1","h2"],"body":{"h1":[1,2,None,None],"h2":[10,None,30,40]},"length":4})
-    df2 = df.dropna(subset=["h1"])
-    df3 = df.dropna(subset=["h2"])
-    df4 = df.dropna(subset=["h2","h1"])
-    assert df2.length == 2
-    assert df3.length == 3
-    assert df4.length == 1
+    def test_dropna(self):
+        print "testing dropna..."
+        df = pd.DataFrame({"head":["h1","h2"],"body":{"h1":[1,2,None,None],"h2":[10,None,30,40]},"length":4})
+        df2 = df.dropna(subset=["h1"])
+        df3 = df.dropna(subset=["h2"])
+        df4 = df.dropna(subset=["h2","h1"])
+        assert df2.length == 2
+        assert df3.length == 3
+        assert df4.length == 1
 
-def test_dataframe():
-    print "testing dataframe...!"
-    df = pd.DataFrame({"head":["h1","h2"],"body":{"h1":[1,2,3,4],"h2":[10,20,30,40]},"length":4})
-    assert df.length == 4
-    assert df[0] == (1,10)
-    assert df.h1[0] == 1
-    assert df.h2[2] == 30
-    assert df["h1"][1] == 2
-    assert df["h2"][1] == 20
+    def test_dataframe(self):
+        print "testing dataframe..."
+        df = pd.DataFrame({"head":["h1","h2"],"body":{"h1":[1,2,3,4],"h2":[10,20,30,40]},"length":4})
+        assert df.length == 4
+        assert df[0] == (1,10)
+        assert df.h1[0] == 1
+        assert df.h2[2] == 30
+        assert df["h1"][1] == 2
+        assert df["h2"][1] == 20
+
+def do_test(t,name):
+    try:
+        getattr(t,name)()
+    except Exception as e:
+        print e
+        print "-- ERROR -- There was an error running test '%s'"%name
 
 def run():
+    t = Test()
     print "begin testing"
-    test_dataframe()
-    test_dropna()
+    do_test(t,"test_dataframe")
+    do_test(t,"test_index")
+    do_test(t,"test_dropna")
     print "done"
 
