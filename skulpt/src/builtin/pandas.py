8b65e51565856eea1bf9c2dcc7dce583c2b00364
Orion Henry <orion.henry@gmail.com>
2015-12-10 00:20:17 -0600

    ugly workaround for the convert issues

--- skulpt/src/builtin/pandas.py
+++ skulpt/src/builtin/pandas.py
@@ -1,5 +1,6 @@
 
 import js
+import json
 
 def do_math(func,data):
     if len(data) > 0 and (type(data[0]) == int or type(data[0]) == float):
@@ -121,6 +122,10 @@ class DataFrame:
         d._idx = idx
         return d
 
+    @staticmethod
+    def from_csv(path,**kargs):
+        return read_csv(path)
+
     def __getitem__(self,i):
         if (type(i) is str):
             return Series(self._data,i,self._sort,self._idx)
@@ -158,9 +163,6 @@ class DataFrame:
             if key == "subset":
                 new_idx = [x for x in new_idx if all([self._data[c][x] != None for c in cols])]
         return self._reindex(new_idx)
-
-    def from_csv(path,**kargs):
-        return read_csv(path)
 
     def groupby(self,by):
         return GroupBy(self,by)
@@ -237,7 +239,7 @@ def read_csv(filename, header=None, names=None):
     if header is None and names is not None:
         header = names
 
-    data = js.globals.parse_raw_data(filename,header,names)
+    data = json.loads(str(js.globals.parse_raw_data(filename,header,names)))
 
     return DataFrame.from_data(data)
 
