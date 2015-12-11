c8fdb21f58530661dfa4ac6dc37d66a52f4ec0ee
Orion Henry <orion.henry@gmail.com>
2015-12-10 21:34:45 -0600

    starter notebook working

--- src/base.py
+++ src/base.py
@@ -7,14 +7,12 @@ def mark(n):
 
 
 def render(cell,val):
-    print "about to convert 2 %s" % val
     if hasattr(val, 'to_js'):
         val2 = ["html", val.to_js()]
     elif type(val) == list:
         val2 = ["list", val]
     else:
         val2 = ["text", str(val)]
-    print "val2 = %s" % val2
     p = pt.get_plots()
     if (len(p) > 0):
         js.globals['PLOTS'][cell] = js.convert(p)
